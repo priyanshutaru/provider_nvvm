bd38ec7f31dfaffd1b27dc90e0d9b00b2aecc073
LENV <technicalpriyanshutaru@gmail.com>
2023-07-27 09:01:55 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+// ignore_for_file: unused_import
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_nvvm/utils/routes/routes.dart';
+import 'package:provider_nvvm/utils/routes/routes_name.dart';
+import 'package:provider_nvvm/view_model/auth_view_model.dart';
+import 'package:provider_nvvm/view_model/user_view_model.dart';
+// import 'package:mvvm/utils/routes/routes.dart';
+// import 'package:mvvm/utils/routes/routes_name.dart';
+// import 'package:mvvm/view/login_view.dart';
+// import 'package:mvvm/view_model/auth_view_model.dart';
+// import 'package:mvvm/view_model/user_view_model.dart';
+// import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => AuthViewModel()),
+          ChangeNotifierProvider(create: (_) => UserViewModel())
+        ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        initialRoute: RoutesName.splash,
+        onGenerateRoute: Routes.generateRoute,
+      ),
+    );
+  }
+}
+
---
+++ lib/respository/auth_repository.dart
@@ -0,0 +1,47 @@
+
+
+// ignore_for_file: unused_import
+
+import 'dart:convert';
+
+import 'package:provider_nvvm/data/network/NetworkApiService.dart';
+import 'package:provider_nvvm/res/app_url.dart';
+
+import '../data/network/BaseApiServices.dart';
+
+// import 'package:http/http.dart';
+// import 'package:mvvm/data/network/BaseApiServices.dart';
+// import 'package:mvvm/data/network/NetworkApiService.dart';
+// import 'package:mvvm/res/app_url.dart';
+
+class AuthRepository  {
+
+  BaseApiServices _apiServices = NetworkApiService() ;
+
+
+  Future<dynamic> loginApi(dynamic data )async{
+
+    try{
+
+      dynamic response = await _apiServices.getPostApiResponse(AppUrl.loginEndPint, data);
+      return response ;
+
+    }catch(e){
+      throw e ;
+    }
+  }
+
+  Future<dynamic> signUpApi(dynamic data )async{
+
+    try{
+
+      dynamic response = await _apiServices.getPostApiResponse(AppUrl.registerApiEndPoint, data);
+      return response ;
+
+
+    }catch(e){
+      throw e ;
+    }
+  }
+
+}
